fa9a36d8fc581baea248ecaddec4335014c14089
jagernau <stepanenkov25@gmail.com>
2024-09-28 22:03:15 +0200

    установил не ассинхронную версию

--- bot.py
+++ bot.py
@@ -1,43 +1,27 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
-from datetime import datetime
-import logging
+import schedule
+import time
+from telegram import Bot
 import config
 
-# Включаем логирование
-logging.basicConfig(level=logging.INFO)
+TOKEN = str(config.TOKEN)
+GROUP_ID = str(config.GROUP_ID)
 
-# Замените 'YOUR_TOKEN' на ваш токен
-API_TOKEN = config.TOKEN
+bot = Bot(token=str(TOKEN))
 
-# Инициализация бота и диспетчера
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+def send_morning_message():
+    bot.send_message(GROUP_ID, 'Доброе утро')
 
-# ID группы, куда будут отправляться сообщения
-GROUP_ID = config.GROUP_ID  # Замените на ваш ID группы
+def send_night_message():
+    bot.send_message(GROUP_ID, 'Доброй ночи')
 
-async def send_morning_message():
-    while True:
-        now = datetime.now()
-        if now.hour == 8 and now.minute == 0:  # Время отправки "Доброе утро"
-            await bot.send_message(GROUP_ID, "Доброе утро!")
-            await asyncio.sleep(60)  # Ждем минуту перед следующей проверкой
-        await asyncio.sleep(30)  # Проверяем каждые 30 секунд
 
-async def send_night_message():
-    while True:
-        now = datetime.now()
-        if now.hour == 22 and now.minute == 0:  # Время отправки "Спокойной ночи"
-            await bot.send_message(GROUP_ID, "Спокойной ночи!")
-            await asyncio.sleep(60)  # Ждем минуту перед следующей проверкой
-        await asyncio.sleep(30)  # Проверяем каждые 30 секунд
+# schedule.every().day.at("08:00").do(send_morning_message)
+# schedule.every().day.at("22:00").do(send_night_message)
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
-@dp.on_startup()
-async def on_startup(_):
-    asyncio.create_task(send_morning_message())
-    asyncio.create_task(send_night_message())
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+send_morning_message()
+time.sleep(10)
+send_night_message()
