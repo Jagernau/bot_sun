9fd04089ac2a3141365e6bac7b5e522ed71a6f61
jagernau <stepanenkov25@gmail.com>
2024-10-07 17:08:18 +0300

    добавил логгирование приостановленные объекты

--- bot.py
+++ bot.py
@@ -27,12 +27,39 @@ def send_add_obj_yesterday_message():
             f'{yesterday_with_fix_time}',
             f'{evening_time}'
             )
+    if len(data_db) >= 1:
+        hf.create_excel_file(data_db, filename)
 
-    hf.create_excel_file(data_db, filename)
+        bot.send_document(GROUP_ID, open(f'{FILES_DIR}/{filename}.xls', 'rb'))
 
-    bot.send_document(GROUP_ID, open(f'{FILES_DIR}/{filename}.xls', 'rb'))
+        bot.send_message(GROUP_ID, f'{filename}')
+    else:
+        pass
 
-    bot.send_message(GROUP_ID, f'{filename}')
+
+def send_del_stop_obj_yesterday_message():
+    """ 
+    отправляет таблицу с удалёнными и приостановлеными объектами в excel
+    """
+    yesterday = hf.get_yesterday()
+    filename = f"Объекты удалённые и приостановленные за {yesterday}"
+    yesterday_with_fix_time = str(yesterday) + " 12:41:04"
+    evening_time = datetime.now() + timedelta(days=1)
+
+    data_db = crud.get_del_stop_obj(
+            f'{yesterday_with_fix_time}',
+            f'{evening_time}'
+            )
+
+    if len(data_db) >= 1:
+
+        hf.create_excel_file(data_db, filename)
+
+        bot.send_document(GROUP_ID, open(f'{FILES_DIR}/{filename}.xls', 'rb'))
+
+        bot.send_message(GROUP_ID, f'{filename}')
+    else:
+        pass
 
 
 def send_add_obj_week_message():
@@ -42,10 +69,12 @@ def send_add_obj_week_message():
 
     bot.send_message(GROUP_ID, 'Отчёт по добавленным объектам за неделю\n')
 
-schedule.every().day.at("09:26").do(send_add_obj_yesterday_message)
-#schedule.every().day.at("22:00").do(send_night_message)
 
-#send_add_obj_yesterday_message()
+schedule.every().day.at("09:26").do(send_add_obj_yesterday_message)
+schedule.every().day.at("09:27").do(send_del_stop_obj_yesterday_message)
+
+# send_add_obj_yesterday_message()
+# send_del_stop_obj_yesterday_message()
 
 while True:
     schedule.run_pending()
--- database/crud.py
+++ database/crud.py
@@ -1,12 +1,13 @@
 from os.path import join
 import database.mysql_models as models
 from database.bd_conectors import MysqlDatabase
-from sqlalchemy import func
+from sqlalchemy import func, or_, and_
 
 
 def get_log_obj(date_start, date_end):
     """
     Отдаёт логи по объектам за периуд времени
+    Добавленные
     """
     db = MysqlDatabase()
     session = db.session
@@ -28,3 +29,31 @@ def get_log_obj(date_start, date_end):
 
 
 
+def get_del_stop_obj(date_start, date_end):
+    """
+    Отдаёт логи по объектам за периуд времени
+    Удалённые и деактивированные.
+    """
+    db = MysqlDatabase()
+    session = db.session
+    result = session.query(
+            models.Contragent.ca_name,
+            models.MonitoringSystem.mon_sys_name,
+            models.GlobalLogging.old_value,
+                           ).outerjoin(
+                models.Contragent, models.GlobalLogging.contragent_id == models.Contragent.ca_id
+                ).outerjoin(
+                        models.MonitoringSystem, models.GlobalLogging.sys_id == models.MonitoringSystem.mon_sys_id
+                        ).filter(
+                models.GlobalLogging.change_time.between(date_start, date_end),
+                models.GlobalLogging.section_type=="object",
+                or_(
+                    models.GlobalLogging.action=="delete",
+                    and_(
+                        models.GlobalLogging.action == "update",
+                        models.GlobalLogging.new_value.like("%_приост%"),
+                    )
+                )
+            ).all()
+    session.close()
+    return result
